a89fa13c246be63bba178ceb8f2f32609859d509
Samrat <samratbiswas460@gmail.com>
2025-07-26 23:04:02 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -30,15 +30,15 @@ function App() {
 
       <div className="App">
         <div className="left">
-          <h4>What is my IPv4 address?</h4>
+          <h2>What is my IPv4 address?</h2>
           <h1 id="ip">{ipDetails.ip}</h1>
 
-          <h4>Approximate location:</h4>
+          <h2>Approximate location:</h2>
           <p>
             {ipDetails.city}, {ipDetails.region}, {ipDetails.country_name}.
           </p>
 
-          <h4>Internet Service Provider (ISP):</h4>
+          <h2>Internet Service Provider (ISP):</h2>
           <p>{ipDetails.org}</p>
         </div>
 
